fe489366f52724e16a56abe03f2e77132560f4c5
lanyiping <lanyip@outlook.com>
2025-03-23 17:09:34 +0800

    unionId改为请求体获取

--- src/main/java/com/tencent/service/impl/UsersServiceImpl.java
+++ src/main/java/com/tencent/service/impl/UsersServiceImpl.java
@@ -5,7 +5,10 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tencent.model.Users;
 import com.tencent.service.UsersService;
 import com.tencent.mapper.UsersMapper;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.Enumeration;
 
 /**
 * @author lanyiping
@@ -17,7 +20,16 @@ public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users>
     implements UsersService{
 
     @Override
-    public Users getUserInfo(String unionId) {
+    public Users getUserInfo(HttpServletRequest request) {
+        // 获取指定header参数的unionId
+        String unionId = request.getHeader("X-WX-UNIONID");
+        // 获取全部header参数的名称和值
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            System.out.println(headerName + ": " + headerValue);
+        }
         QueryWrapper<Users> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("union_id",unionId);
         return this.getOne(queryWrapper);
